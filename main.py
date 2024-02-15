cba51ced5d1d774b9d2577be9dc63cb66fa28255
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2024-02-15 15:22:45 +0000

    dsqdqDFsqdfQSDSqd

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 import pandas as pd
 import json
+import config
 
-app_id = "724647526404032"
-app_token = "949bf118e7fd1690a47bd036e7d4c95c"
-page_id = "100069074211251"
-post_id = "716403620672105"
-access_token = "EAAKTECZAmD8ABOy4z6ZAwEBw384lEEx8lP6C3tB1DLRsH5ZCLdcSJu5y1LB8NVNrULZAg8Efs8ZAIpdYdyr5Qd8vAbfW3XZCoC5cCxKTtogVVAq6SagpGXScwtb9VexnGzEDTVNuaXBhaHpIJKQJGZB6OdwuIZAi44kZApk3FoNAt17V5RZBPs0wb0RnXUwR2pfq7He2hJGKkRDxR4CZCuy2sTzmKoXHSeoJwqpZBZABGnfnmAQZDZD"
-
+app_id = config.app_id
+app_token = config.app_token
+page_id = config.page_id
+post_id = config.post_id
+access_token = config.access_token
 
 url = f"https://graph.facebook.com/v19.0/{page_id}_{post_id}/comments?limit=250&fields=from,message,like_count&access_token={access_token}"
 
