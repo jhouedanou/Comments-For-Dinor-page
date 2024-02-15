1fda09ba5245803f3afa60bd8ba4087199e8285a
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2024-02-15 15:17:52 +0000

    test

--- main.py
+++ main.py
@@ -2,23 +2,20 @@ import requests
 import pandas as pd
 import json
 
-
+app_id = "724647526404032"
+app_token = "949bf118e7fd1690a47bd036e7d4c95c"
 page_id = "100069074211251"
 post_id = "716403620672105"
-access_token = "EAAKTECZAmD8ABO0DpiyF0TKihwn5sRuGIyTbFwZBjN1ubbve5waxSKqTRHSTQtemOPjJBSM2qNus2jyVDckN5RTzMy8xiceZA3tjXHhxbYQrvTXFXxgsRB01J8cNVBLgxsVtHM5iIf3DoNBgwrq5gspeGXI28ZCOIT2GwiRxdJ6wlAtIvfizpydB8E6ECTzZBXtJaYvK2e44SWvfC7K03kZAgcz56edkhpti961rcn"
+access_token = "EAAKTECZAmD8ABOy4z6ZAwEBw384lEEx8lP6C3tB1DLRsH5ZCLdcSJu5y1LB8NVNrULZAg8Efs8ZAIpdYdyr5Qd8vAbfW3XZCoC5cCxKTtogVVAq6SagpGXScwtb9VexnGzEDTVNuaXBhaHpIJKQJGZB6OdwuIZAi44kZApk3FoNAt17V5RZBPs0wb0RnXUwR2pfq7He2hJGKkRDxR4CZCuy2sTzmKoXHSeoJwqpZBZABGnfnmAQZDZD"
 
 
-url = f"https://graph.facebook.com/v19.0/{page_id}_{post_id}/comments?limit=500&fields=from,message,like_count&access_token={access_token}"
+url = f"https://graph.facebook.com/v19.0/{page_id}_{post_id}/comments?limit=250&fields=from,message,like_count&access_token={access_token}"
 
-response = requests.request("GET", url)
-
-# save name, time, message in excel file
-data = json.loads(response.text)
-
-print(data)
+response = requests.get(url)
+json_data = response.content
+data = json.loads(json_data)
 
 
-# create object with only name, time, message
 def get_comment(comment):
     return {
         "name": comment["id"],
